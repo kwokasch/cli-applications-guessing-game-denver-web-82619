d7290a0986a1ed3c73a79ecd46e85338e0dce36e
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-14 15:36:05 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   input = gets.chomp 
   if input == "exit"
     puts "Goodbye!"
-  elsif input == comp_guess
+  elsif input = comp_guess
     puts "You guessed the correct number!"
   else 
     puts "Sorry! The computer guessed #{comp_guess}."
