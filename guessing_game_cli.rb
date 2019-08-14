007ba9d5f62d1be3f294faf3ba8e7c91097851a2
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-14 15:54:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,12 @@
 def run_guessing_game
   comp_guess = rand(6) + 1   
   #=> returns random integer from 1 to 6 (range will not work in this lab)
+  
   roll = comp_guess.to_s
+  #=> convert integer in comp_guess to a string 
+  
   input = gets.chomp 
+  #=> assigns user's input to "input" and removes newline character with .chomp 
   
   if input == roll
     puts "You guessed the correct number!"
