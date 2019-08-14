0261d5cbfdc2d9ac16d53eae38788006603bd9b1
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-14 15:32:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,7 @@
 def run_guessing_game
   rand(6) + 1   
   #=> returns random integer from 1 to 6 (range will not work in this lab)
-  input = gets 
+  input = gets.chomp 
+  if input = "exit"
+    puts "Goodbye!"
 end
