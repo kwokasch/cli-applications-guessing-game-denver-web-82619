ea96fe9a916e62299c3fd1f0b58169c811dc85c6
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-14 15:35:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   comp_guess = rand(6) + 1   
   #=> returns random integer from 1 to 6 (range will not work in this lab)
   input = gets.chomp 
-  if input = "exit"
+  if input == "exit"
     puts "Goodbye!"
   elsif input == comp_guess
     puts "You guessed the correct number!"
