ec1df8f32125c4a70ace79e6cc2b869ef173cbaf
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-14 15:49:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,6 +9,6 @@ def run_guessing_game
   elsif input == "exit"
     puts "Goodbye!"
   else  
-    puts "Sorry! The computer guessed #{comp_guess.chomp}."
+    puts "Sorry! The computer guessed #{comp_guess}."
   end
 end
