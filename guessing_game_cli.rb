35bbf2834d0b61ada467c627240d9f79bdc7f3eb
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-14 15:50:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   comp_guess = rand(6) + 1   
   #=> returns random integer from 1 to 6 (range will not work in this lab)
-  roll = comp_guess.chomp.to_s
+  roll = comp_guess.to_s
   input = gets.chomp 
   
   if input == comp_guess
